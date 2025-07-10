37be893fee32a58920ac3d37161aa57bf721e1b1
Shawn Kang <csm06063@naver.com>
2025-07-11 00:55:11 +0900

    refactor: Removed unnecessary routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,21 +3,10 @@ import TableListPage from '../page/TableListPage/TableListPage';
 import TableOverview from '../page/TableOverviewPage/TableOverview';
 import TableComposition from '../page/TableComposition/TableComposition';
 import ErrorBoundaryWrapper from '../components/ErrorBoundary/ErrorBoundaryWrapper';
-import ProtectedRoute from './ProtectedRoute';
-import OAuth from '../page/OAuthPage/OAuth';
-import ReactGA from 'react-ga4';
 import NotFoundPage from '../components/ErrorBoundary/NotFoundPage';
-import BackActionHandler from '../components/BackActionHandler';
 import TimerPage from '../page/TimerPage/TimerPage';
-import LandingPage from '../page/LandingPage/LandingPage';
-import TableSharingPage from '../page/TableSharingPage/TableSharingPage';
 
 const routesConfig = [
-  {
-    path: '/home',
-    element: <LandingPage />,
-    requiresAuth: false,
-  },
   {
     path: '/',
     element: <TableListPage />,
@@ -39,16 +28,6 @@ const routesConfig = [
     requiresAuth: false,
   },
   {
-    path: '/oauth',
-    element: <OAuth />,
-    requiresAuth: false,
-  },
-  {
-    path: '/share',
-    element: <TableSharingPage />,
-    requiresAuth: false,
-  },
-  {
     path: '*',
     element: <NotFoundPage />,
     requiresAuth: false,
@@ -57,26 +36,12 @@ const routesConfig = [
 
 const router = createBrowserRouter([
   {
-    element: (
-      <>
-        <ErrorBoundaryWrapper />
-        <BackActionHandler />
-      </>
-    ),
+    element: <ErrorBoundaryWrapper />,
     children: routesConfig.map((route) => ({
       ...route,
-      element: route.requiresAuth ? (
-        <ProtectedRoute>{route.element}</ProtectedRoute>
-      ) : (
-        route.element
-      ),
+      element: route.element,
     })),
   },
 ]);
-
-// 라우트 변경 시 Google Analytics 이벤트 전송
-router.subscribe(({ location }) => {
-  ReactGA.send({ hitType: 'pageview', page: location.pathname });
-});
 
 export default router;
