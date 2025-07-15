f1f7345410c7cdae309b83a520b9c7fee0fb9c27
Shawn Kang <shineksu813@outlook.com>
2025-07-15 18:45:38 +0900

    refactor: Applied new async request on TableListPage

--- src/page/TableListPage/TableListPage.tsx
+++ src/page/TableListPage/TableListPage.tsx
@@ -5,17 +5,21 @@ import Table from './components/Table';
 import HeaderTitle from '../../components/HeaderTitle/HeaderTitle';
 import repository from '../../repositories/IPCDebateTableRepository';
 import { UUID } from 'crypto';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import useAsyncRequest from '../../repositories/useAsyncRequest';
+import ErrorIndicator from '../../components/async/ErrorIndicator';
+import LoadingIndicator from '../../components/async/LoadingIndicator';
 
 export default function TableListPage() {
   const repo = repository;
-  const [data, setData] = useState<DebateTableData[]>([]);
+  const {
+    data: getAllTablesData,
+    error: getAllTablesError,
+    isLoading: getAllTablesLoading,
+    execute: getAllTables,
+  } = useAsyncRequest(repo.getAllTables);
   const navigate = useNavigate();
 
-  const getAllTables = async () => {
-    const response = await repository.getAllTables();
-    setData(response);
-  };
   // TODO: have to delete the query param 'type'
   const onEdit = (tableId: UUID) => {
     navigate(`/composition?mode=edit&tableId=${tableId}&type=CUSTOMIZE`);
@@ -31,7 +35,7 @@ export default function TableListPage() {
 
   useEffect(() => {
     getAllTables();
-  }, []);
+  }, [getAllTables]);
 
   return (
     <DefaultLayout>
@@ -44,29 +48,39 @@ export default function TableListPage() {
       </DefaultLayout.Header>
 
       <DefaultLayout.ContentContainer>
-        <div className="flex max-w-[1140px] flex-wrap justify-start">
-          {/** Button that adds new table */}
-          <button
-            onClick={() => navigate(`/composition?mode=add`)}
-            className="m-5 h-[220px] w-[340px] rounded-[28px] bg-neutral-200 shadow-lg duration-200 hover:scale-105"
-          >
-            <h1 className="text-[100px] font-light text-neutral-500">+</h1>
-          </button>
+        {getAllTablesLoading && <LoadingIndicator />}
+        {!getAllTablesLoading && getAllTablesError && (
+          <ErrorIndicator
+            message="데이터를 불러오지 못했어요. 다시 시도할까요?"
+            onClickRetry={() => getAllTables()}
+          />
+        )}
 
-          {/** All tables */}
-          {data &&
-            data.map((table: DebateTableData, idx: number) => (
-              <Table
-                key={idx}
-                id={table.info.id}
-                name={table.info.name}
-                agenda={table.info.agenda}
-                onDelete={() => onDelete(table.info.id)}
-                onEdit={() => onEdit(table.info.id)}
-                onClick={() => onClick(table.info.id)}
-              />
-            ))}
-        </div>
+        {!getAllTablesLoading && !getAllTablesError && (
+          <div className="flex max-w-[1140px] flex-wrap justify-start">
+            {/** Button that adds new table */}
+            <button
+              onClick={() => navigate(`/composition?mode=add`)}
+              className="m-5 h-[220px] w-[340px] rounded-[28px] bg-neutral-200 shadow-lg duration-200 hover:scale-105"
+            >
+              <h1 className="text-[100px] font-light text-neutral-500">+</h1>
+            </button>
+
+            {/** All tables */}
+            {getAllTablesData &&
+              getAllTablesData.map((table: DebateTableData, idx: number) => (
+                <Table
+                  key={idx}
+                  id={table.info.id}
+                  name={table.info.name}
+                  agenda={table.info.agenda}
+                  onDelete={() => onDelete(table.info.id)}
+                  onEdit={() => onEdit(table.info.id)}
+                  onClick={() => onClick(table.info.id)}
+                />
+              ))}
+          </div>
+        )}
       </DefaultLayout.ContentContainer>
     </DefaultLayout>
   );
